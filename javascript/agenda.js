f0ffdd8658ba9f3ad5ec4c86c6d24a9e71d30491
edmarketing <edjmaria2021@gmail.com>
2023-07-29 12:09:20 -0300

    Atualizando Projeto

--- javascript/agenda.js
+++ javascript/agenda.js
@@ -6,3 +6,19 @@ const btn_pesquisar=document.querySelector("#btn_pesquisar");
 const btn_gestao=document.querySelector("#btn_gestao");
 const btn_sobre=document.querySelector("#btn_sobre");
 const principal=document.querySelector("#principal");
+
+btn_home.addEventListener("click",(evt)=>{
+    window.open("./home.html","if_principal");
+});
+btn_novo.addEventListener("click",(evt)=>{
+    window.open("/novo.html","if_principal");
+});
+btn_pesquisar.addEventListener("click",(evt)=>{
+    window.open("./pesquisar.html","if_principal");
+});
+btn_gestao.addEventListener("click",(evt)=>{
+    window.open("./gestao.html","if_principal");
+});
+btn_sobre.addEventListener("click",(evt)=>{
+    window.open("./sobre.html","if_principal");
+});
