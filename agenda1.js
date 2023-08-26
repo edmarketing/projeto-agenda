316dc3beedbd7bdf86c52184fc381083e890d5fe
edmarketing <edjmaria2021@gmail.com>
2023-08-26 20:50:14 -0300

    atualizando

---
+++ agenda1.js
@@ -0,0 +1,12 @@
+const cabecalho=document.querySelector("#cabecalho");
+const menu=document.querySelector("#menu");
+const btn_home=document.querySelector("#btn_home");
+const btn_novo=document.querySelector("#btn_novo");
+const btn_pesquisar=document.querySelector("#btn_pesquisar");
+const btn_gestao=document.querySelector("#btn_gestao");
+const btn_sobre=document.querySelector("#btn_sobre");
+const principal=document.querySelector("#principal");
+
+btn_home.addEventListener("click",(evt)=>{
+    window.open("home.html","if_principal");
+});
