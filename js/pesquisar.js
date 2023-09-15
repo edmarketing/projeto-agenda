3c8d3719f3abb4ec84c6ebefd65fabd69865375c
edmarketing <edjmaria2021@gmail.com>
2023-09-15 12:28:24 -0300

    Atualizando projeto

--- js/pesquisar.js
+++ js/pesquisar.js
@@ -2,9 +2,20 @@ const btn_pesq = document.querySelector("#btn_pesq");
 const f_txtpesq = document.querySelector("#f_txtpesq");
 
 btn_pesq.addEventListener("click",(evt)=>{
-    if(f_txtpesq.value==""){
+    const valorPesq = f_txtpesq.value; 
+    if(valorPesq==""){
         alert("Digite uma pesquisa!");
         f_txtpesq.focus();
         return
     }
+    const f_pesq = document.querySelector("input[name=f_pesq]:checked").value;
+    if (f_pesq=="id") {
+        
+    } else if (f_pesq=="nome") {
+        
+    } else if (f_pesq=="nasc") {
+        
+    } else if (f_pesq=="email") {
+        
+}
 });
