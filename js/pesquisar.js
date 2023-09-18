1319a294c62919f0fa14a7b17e1b896c9f036840
edmarketing <edjmaria2021@gmail.com>
2023-09-18 18:08:04 -0300

    atualizando

--- js/pesquisar.js
+++ js/pesquisar.js
@@ -4,15 +4,17 @@ const f_txtpesq = document.querySelector("#f_txtpesq");
 btn_pesq.addEventListener("click",(evt)=>{
     const valorPesq = f_txtpesq.value; 
     if(valorPesq==""){
+        console.log(valorPesq)
         alert("Digite uma pesquisa!");
         f_txtpesq.focus();
+
         return
     }
-    const f_pesq = document.querySelector("input[name=f_pesq]:checked").value;
-    const entpoint = `http://127.0.0.1:1880/pesquisarcontatos/${f_pesq}/${valorPesq}`;
-    fetch(entpoint);
-    .then(res=>res.json());
+    const f_pesq = document.querySelector("input[name=f_pesq]:checked").value;   
+    const entpoint=`http://127.0.0.1:1880/pesquisarcontatos/${f_pesq}/${valorPesq}`;
+    fetch(entpoint)
+    .then(res=>res.json())
     .then(res=>{
         console.log(res)
-    })    
+    })
 });
